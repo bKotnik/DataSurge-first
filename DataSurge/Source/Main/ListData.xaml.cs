3f0815fb16c9aa6e4e5ab2edfdcd7e898904a2a0
Benjamin Kotnik <61419336+bKotnik@users.noreply.github.com>
2020-12-30 19:54:35 +0100

    Fixed Listdata window and updated loading UC

--- DataSurge/Source/Main/ListData.xaml.cs
+++ DataSurge/Source/Main/ListData.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Xml.Serialization;
 
@@ -23,81 +24,90 @@ namespace DataSurge.Main
         private void function()
         {
             int i = 1;
-            Stream stream = File.OpenRead(Environment.CurrentDirectory + "\\Data.xml");
-            XmlSerializer xs = new XmlSerializer(typeof(ObservableCollection<DataClass>));
 
             try
             {
-                using (stream)
+                foreach (DataClass data in Utility.ListData)
                 {
-                    Utility.ListData = (ObservableCollection<DataClass>)xs.Deserialize(stream);
-
-                    foreach (DataClass data in Utility.ListData)
+                    if (WhichButton.buttonContent == "EMAILS" || WhichButton.buttonContent == "E-NASLOVI")
                     {
-                        if (WhichButton.buttonContent == "EMAILS" || WhichButton.buttonContent == "E-NASLOVI")
-                        {
-                            Title = WhichButton.listLabel + "s";
-                            label.Text = WhichButton.listLabel;
+                        Title = WhichButton.listLabel + "s";
+                        label.Text = WhichButton.listLabel;
 
+                        if(data.Email != "/")
+                        {
                             ListDataClass ldc = new ListDataClass();
                             ldc.Number = i;
                             ldc.Data = data.Email;
                             Utility.LDClass.Add(ldc);
                         }
+                    }
 
-                        else if (WhichButton.buttonContent == "USERNAMES" || WhichButton.buttonContent == "UPORABNIŠKA IMENA")
+                    else if (WhichButton.buttonContent == "USERNAMES" || WhichButton.buttonContent == "UPORABNIŠKA IMENA")
+                    {
+                        Title = WhichButton.listLabel + "s";
+                        label.Text = WhichButton.listLabel;
+
+                        if(data.Username != "/")
                         {
-                            Title = WhichButton.listLabel + "s";
-                            label.Text = WhichButton.listLabel;
-
                             ListDataClass ldc = new ListDataClass();
                             ldc.Number = i;
                             ldc.Data = data.Username;
                             Utility.LDClass.Add(ldc);
                         }
+                    }
 
-                        else if (WhichButton.buttonContent == "PASSWORDS" || WhichButton.buttonContent == "GESLA")
+                    else if (WhichButton.buttonContent == "PASSWORDS" || WhichButton.buttonContent == "GESLA")
+                    {
+                        Title = WhichButton.listLabel + "s";
+                        label.Text = WhichButton.listLabel;
+
+                        ListDataClass ldc = new ListDataClass();
+
+                        if (data.Password != "/")
                         {
-                            Title = WhichButton.listLabel + "s";
-                            label.Text = WhichButton.listLabel;
-
-                            ListDataClass ldc = new ListDataClass();
                             ldc.Number = i;
                             ldc.Data = data.Password;
                             Utility.LDClass.Add(ldc);
                         }
+                    }
 
-                        else if (WhichButton.buttonContent == "OTHER" || WhichButton.buttonContent == "DRUGO")
+                    else if (WhichButton.buttonContent == "OTHER" || WhichButton.buttonContent == "DRUGO")
+                    {
+                        Title = WhichButton.listLabel;
+                        label.Text = WhichButton.listLabel;
+
+
+                        if(data.Other != "/")
                         {
-                            Title = WhichButton.listLabel;
-                            label.Text = WhichButton.listLabel;
-
                             ListDataClass ldc = new ListDataClass();
                             ldc.Number = i;
                             ldc.Data = data.Other;
                             Utility.LDClass.Add(ldc);
                         }
+                    }
 
-                        else if (WhichButton.buttonContent == "NOTES" || WhichButton.buttonContent == "ZAPISKI")
+                    else if (WhichButton.buttonContent == "NOTES" || WhichButton.buttonContent == "ZAPISKI")
+                    {
+                        Title = WhichButton.listLabel + "s";
+                        label.Text = WhichButton.listLabel;
+
+                        if(data.Note != "/")
                         {
-                            Title = WhichButton.listLabel + "s";
-                            label.Text = WhichButton.listLabel;
-
                             ListDataClass ldc = new ListDataClass();
                             ldc.Number = i;
                             ldc.Data = data.Note;
                             Utility.LDClass.Add(ldc);
                         }
-
-                        i++;
                     }
+
+                    i++;
                 }
             }
 
             catch
             {
-                if (stream.Length != 0)
-                    _ = MessageBox.Show("Error occurred when trying to load data", "Error loading data", MessageBoxButton.OK, MessageBoxImage.Error);
+                _ = MessageBox.Show("Error occurred when trying to load data", "Error loading data", MessageBoxButton.OK, MessageBoxImage.Error);
             }
 
             lvListData.ItemsSource = Utility.LDClass;
